fc351c65c4fdf7b046c2c7542d2c2abc1c4762b3
Dharunkumar <dharunkumarsengottuvelu@gmail.com>
2026-02-20 22:48:12 +0530

    Add files via upload

---
+++ Basic Programs/Bp1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bp1 {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+
+        int basic=scan.nextInt();
+        int hra = scan.nextInt();
+        int bonus=scan.nextInt();
+        int tax=scan.nextInt();
+        int pf=scan.nextInt();
+
+        int total=basic+hra+bonus-tax-pf;
+
+        System.out.println(total);
+        
+
+
+        
+    }
+    
+     
+    
+}
